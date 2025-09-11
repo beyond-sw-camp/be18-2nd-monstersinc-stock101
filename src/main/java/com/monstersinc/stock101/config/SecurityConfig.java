ee62abdb0b4820a3f6a72ca0c46844fc2c5a45ec
SonHyewon <hyewon.135@daum.net>
2025-09-11 22:57:59 +0900

    [혜원] 게시물 삭제 기능 구현 및 좋아요 등록, 취소 기능 구현

--- src/main/java/com/monstersinc/stock101/config/SecurityConfig.java
+++ src/main/java/com/monstersinc/stock101/config/SecurityConfig.java
@@ -24,13 +24,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth-> auth
-                        .requestMatchers("/api/v1/auth/login").permitAll()
-
-                        // 2) 게시물 등록 회원만
-                        .requestMatchers(HttpMethod.POST, "/api/v1/board/posts").authenticated()
-
-                        // 3) 조회는 공개
-                        .requestMatchers(HttpMethod.GET, "/api/v1/board/posts/**").permitAll()
+                        .requestMatchers("/api/v1/**").permitAll()
 
                         // 4) 뉴스 조회, 클릭카운트 업데이트는 공개
                         .requestMatchers(HttpMethod.POST, "/api/v1/news/**").permitAll()
@@ -40,7 +34,6 @@ public class SecurityConfig {
                         // 나머지 요청은 일단 모두 허용.
                         .anyRequest().permitAll()
                 )
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .logout(logout-> logout.disable());
 
         return http.build();
