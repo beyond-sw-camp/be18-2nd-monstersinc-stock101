3698576fd9e81731932db64abf04b44c46715b31
xxiuan <ysh990915@gmail.com>
2025-09-19 17:15:05 +0900

    [석현] 지표 조회 예외처리 추가, 뉴스 지표 카운트해서 조회

---
+++ src/main/java/com/monstersinc/stock101/exception/message/GlobalExceptionMessage.java
@@ -0,0 +1,25 @@
+package com.monstersinc.stock101.exception.message;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+
+@Getter
+@RequiredArgsConstructor
+public enum GlobalExceptionMessage {
+
+    DUPLICATE_EMAIL("중복된 이메일 입니다.", HttpStatus.CONFLICT),
+
+    USER_NOT_FOUND("유저 정보를 찾을 수 없습니다.",HttpStatus.NOT_FOUND),
+
+    UNAUTHORIZED_TOKEN("유효한 인증 토큰이 없습니다.", HttpStatus.UNAUTHORIZED),
+
+    UNAUTHORIZED_USER("로그인이 필요합니다.", HttpStatus.UNAUTHORIZED),
+
+    INDICATOR_NOT_FOUND("지표 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+
+    private final String message;
+
+    private final HttpStatus status;
+}
