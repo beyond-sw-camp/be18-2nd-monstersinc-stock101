e7f8bbe58609e11b900cf7f34bac31f6f561cc30
pjw1480 <pjw1480@naver.com>
2025-09-23 12:07:44 +0900

    [박진우] 시큐리티 cors설정.

--- src/main/java/com/monstersinc/stock101/config/SecurityConfig.java
+++ src/main/java/com/monstersinc/stock101/config/SecurityConfig.java
@@ -7,12 +7,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +30,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http, LogbackMetrics logbackMetrics) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(customizer ->
+                        customizer.configurationSource(getCorsConfigurationSource()))
                 .authorizeHttpRequests(auth-> auth
                         // 1) 로그인 관련
                         .requestMatchers("/api/v1/auth/login").permitAll()
@@ -67,4 +75,38 @@ public class SecurityConfig {
 
         return new BCryptPasswordEncoder();
     }
+<<<<<<< Updated upstream
 }
+=======
+
+    private static CorsConfigurationSource getCorsConfigurationSource() {
+
+        return (request) -> {
+            // CorsConfiguration 객체를 생성해서 CORS 설정을 한다.
+            CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+            // CORS 요청에서 허용할 출처를 지정한다.
+            // corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174"));
+            corsConfiguration.setAllowedOriginPatterns(List.of("*"));
+
+            // CORS 요청에서 허용할 HTTP 메소드를 지정한다.
+            corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+            // 클라이언트가 요청 시 사용할 수 있는 헤더를 지정한다.
+            corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+            // 클라이언트가 응답에서 접근할 수 있는 헤더를 지정한다.
+            corsConfiguration.setExposedHeaders(List.of("Authorization"));
+
+            // 자격 증명(쿠키, 세션) 허용 여부를 설정한다.
+            corsConfiguration.setAllowCredentials(true);
+
+            // CORS Preflight 요청을 브라우저가 캐싱하는 시간(초 단위)을 설정한다.
+            corsConfiguration.setMaxAge(3600L);
+
+            return corsConfiguration;
+        };
+    }
+
+}
+>>>>>>> Stashed changes
