1405f9a77b7f6f99932b854739cc41c9693b1c05
Wonseok Cho <jws020501@naver.com>
2025-09-24 15:22:07 +0900

    [원석] 주식 가격 불러오는 로직 수정 및 차트기능 추가

--- src/main/java/com/monstersinc/stock101/restclient/stock/model/service/StockRestClientService.java
+++ src/main/java/com/monstersinc/stock101/restclient/stock/model/service/StockRestClientService.java
@@ -1,10 +1,12 @@
 package com.monstersinc.stock101.restclient.stock.model.service;
 
+import java.util.List;
+
+import com.monstersinc.stock101.restclient.stock.model.dto.StockPriceChartDto;
+
 public interface StockRestClientService {
-    String getStockprices();
+    List<StockPriceChartDto> getStockprices(String Ticker);
     String getFinancialInfo(String timeframe);
     String getNews();
     void getOAuthToken();
-    String getWebSocketKey();
-    String sendWSkey();
 }
